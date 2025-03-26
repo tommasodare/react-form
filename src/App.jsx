b27b60d65380c5ef48d7831d15bdde7ab94d56e1
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-26 17:40:20 +0100

    Create Form with Submit Button

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,30 @@ export default function App() {
 
   return (
     <>
-      <div className="container">
+      <div className="container text-center">
+        <h1>Posts</h1>
         {articles.map((article, index) => (
           <div key={index}>{article.title}</div>
         ))}
       </div>
+
+
+      <form className='m-4 text-center'>
+
+        <div className="mb-3">
+          <label htmlFor="" className="form-label">New Titol</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Insert new Title Here"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+      </form>
+
+
     </>
   )
 }
